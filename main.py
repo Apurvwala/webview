536900fe358fb579ada38266bfc6559453e85d4e
Apurvwala <faceapp0011@gmail.com>
2025-07-09 12:33:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from threading import Thread
 from kivy.app import App
 from kivy.uix.label import Label
 import webbrowser
-from app import app as flask_app
+import app as backend_app
 
 def run_flask():
-    flask_app.run(host='0.0.0.0', port=5000)
+    backend_app.app.run(host="0.0.0.0", port=5000)
 
 class FaceApp(App):
     def build(self):
         Thread(target=run_flask, daemon=True).start()
-        webbrowser.open('http://localhost:5000')
-        return Label(text="FaceApp Running...\nPlease wait...")
+        webbrowser.open("http://localhost:5000")
+        return Label(text="Running Face Recognition Web App")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     FaceApp().run()
